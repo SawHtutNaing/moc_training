ef57fcab5c858c551fb2284ea1c332e0dd77d349
SawHtutNaing <sawhtutnaing2004@gmail.com>
2025-05-15 17:03:26 +0630

    dfdsff

---
+++ app/Models/Gallery.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gallery extends Model
+{
+    protected $fillable = ['file_name', 'description', 'batch_id'];
+
+    public function batch()
+    {
+        return $this->belongsTo(Batch::class);
+    }
+}
